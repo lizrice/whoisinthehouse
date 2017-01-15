32a6c47c3a1bde0807446093c11b455658de67a6
Liz Rice <liz@lizrice.com>
2017-01-15 10:36:20 +0000

    3. Add voice intents

--- lambda.py
+++ lambda.py
@@ -9,8 +9,6 @@ http://amzn.to/1LGWsLG
 
 from __future__ import print_function
 import boto3
-import json
-import decimal
 
 # --------------- Helpers that build all of the responses ----------------------
 
@@ -42,84 +40,128 @@ def build_response(session_attributes, speechlet_response):
         'response': speechlet_response
     }
 
-
-# Helper class to convert a DynamoDB item to JSON.
-class DecimalEncoder(json.JSONEncoder):
-    def default(self, o):
-        if isinstance(o, decimal.Decimal):
-            if o % 1 > 0:
-                return float(o)
-            else:
-                return int(o)
-        return super(DecimalEncoder, self).default(o)
-# --------------- Functions that control the skill's behavior ------------------
+# --------------- Interacting with DynamoDB ------------------------------------
+dynamodb = boto3.resource('dynamodb', region_name='eu-west-1')
+table = dynamodb.Table('WhoIsInTheHouse')
 
 def get_names():
     names = ""
 
-    dynamodb = boto3.resource('dynamodb', region_name='eu-west-1')
-    table = dynamodb.Table('WhoIsInTheHouse')
-
     response = table.scan()
 
-    nameCount == len(response['Items'])
+    nameCount = len(response['Items'])
     for idx, item in enumerate(response['Items']):
         names += item['NameId']
         if idx == nameCount - 2:
             names += " and "
-        else if idx != nameCount - 1:
+        elif idx != nameCount - 1:
             names += ", "
 
     return names
 
+def add_name(name):
+    try:
+        response = table.put_item(
+           Item={
+                'NameId': name
+            }
+        ) 
+    except ClientError as e:
+        print(e.response)
+        return e.response['Error']['Code']        
+        
+    return None
+
+def delete_name(name):
+    try:
+        response = table.delete_item(
+           Key={
+                'NameId': name
+            }
+        )    
+    except ClientError as e:
+        print(e.response)
+        return e.response['Error']['Code']        
+
+    return None
+
+# --------------- Functions that control the skill's behavior ------------------
+
+def get_help_response(): 
+    session_attributes = {}
+    card_title = "Help"
+    speech_output = get_names() + " are here in the house. " + \ 
+        "You can tell me if someone has arrived or left and I'll keep the " + \
+        "list up-to-date."
+    should_end_session = False
+    reprompt_text = "Has anyone arrived or left?"
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+    
+    
 def get_welcome_response():
     session_attributes = {}
     card_title = "Welcome"
     speech_output = get_names() + " are here in the house."
-    should_end_session = True
+    should_end_session = False
+    reprompt_text = "Has anyone arrived or left?"
     return build_response(session_attributes, build_speechlet_response(
-        card_title, speech_output, None, should_end_session))
+        card_title, speech_output, reprompt_text, should_end_session))
 
 
 def handle_session_end_request():
     card_title = "Session Ended"
-    speech_output = "Liz and Phil are " \
-                    "here in the house. Happy new year!"
-    # Setting this to true ends the session and exits the skill.
+    speech_output = get_names() +  " are here in the house."
     should_end_session = True
     return build_response({}, build_speechlet_response(
         card_title, speech_output, None, should_end_session))
 
-
 def create_favorite_color_attributes(favorite_color):
     return {"favoriteColor": favorite_color}
 
-
-def set_color_in_session(intent, session):
-    """ Sets the color in the session and prepares the speech to reply to the
-    user.
-    """
-
+def add_name_in_session(intent, session):
     card_title = intent['name']
     session_attributes = {}
-    should_end_session = False
-
-    if 'Color' in intent['slots']:
-        favorite_color = intent['slots']['Color']['value']
-        session_attributes = create_favorite_color_attributes(favorite_color)
-        speech_output = "I now know your favorite color is " + \
-                        favorite_color + \
-                        ". You can ask me your favorite color by saying, " \
-                        "what's my favorite color?"
-        reprompt_text = "You can ask me your favorite color by saying, " \
-                        "what's my favorite color?"
+    speech_output = ""
+    reprompt_text = ""
+    if 'Name' in intent['slots']:
+        name = intent['slots']['Name']['value']
+        error = add_name(name)
+        if error is not None:
+            speech_output = "Sorry, that didn't work. " + error
+            reprompt_text = "Tell me the name of a person who arrived?"
+            should_end_session = False
+        else:
+            speech_output = "Now that " + name + " is here, " + get_names() + " are here in the house."
+            should_end_session = True
     else:
-        speech_output = "I'm not sure what your favorite color is. " \
-                        "Please try again."
-        reprompt_text = "I'm not sure what your favorite color is. " \
-                        "You can tell me your favorite color by saying, " \
-                        "my favorite color is red."
-    return build_response(session_attributes, build_speechlet_response(
+        reprompt_text = "Sorry, I didn't understand. Please try again."
+        should_end_session = False
+    
+    return build_response({}, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+
+def remove_name_in_session(intent, session):
+    card_title = intent['name']
+    session_attributes = {}
+    speech_output = ""
+    reprompt_text = ""
+
+    if 'Name' in intent['slots']:
+        name = intent['slots']['Name']['value']
+        error = delete_name(name)
+        if error is not None:
+            speech_output = "Sorry, that didn't work. " + error
+            reprompt_text = "Tell me the name of a person who left the house?" 
+            should_end_session = False
+        else:
+            speech_output = "Now that " + name + " has gone, " + get_names() + " are here in the house."
+            should_end_session = True
+    else:
+        reprompt_text = "Sorry, I didn't understand. Please try again."
+        should_end_session = False
+    
+    return build_response({}, build_speechlet_response(
         card_title, speech_output, reprompt_text, should_end_session))
 
 
@@ -167,25 +209,25 @@ def on_launch(launch_request, session):
 def on_intent(intent_request, session):
     """ Called when the user specifies an intent for this skill """
 
-    # print("on_intent requestId=" + intent_request['requestId'] +
-    #       ", sessionId=" + session['sessionId'])
+    print("on_intent requestId=" + intent_request['requestId'] +
+          ", sessionId=" + session['sessionId'])
 
     intent = intent_request['intent']
     intent_name = intent_request['intent']['name']
 
-    # Dispatch to your skill's intent handlers
-    # if intent_name == "MyColorIsIntent":
-    #     return set_color_in_session(intent, session)
-    # elif intent_name == "WhatsMyColorIntent":
-    #     return get_color_from_session(intent, session)
-    # elif intent_name == "AMAZON.HelpIntent":
-    #     return get_welcome_response()
-    # elif intent_name == "AMAZON.CancelIntent" or intent_name == "AMAZON.StopIntent":
-    #     return handle_session_end_request()
-    # else:
-    #     raise ValueError("Invalid intent")
+    if intent_name == "WhoIsInTheHouse":
+        return get_welcome_response()
+    elif intent_name == "ArrivedInTheHouse":
+        return add_name_in_session(intent, session)
+    elif intent_name == "LeftTheHouse":
+        return remove_name_in_session(intent, session)
+    elif intent_name == "AMAZON.CancelIntent" or intent_name == "AMAZON.StopIntent":
+        return handle_session_end_request()
+    elif intent_name == "AMAZON.HelpIntent":
+        return get_welcome_response()
+    else:
+         raise ValueError("Invalid intent")    
 
-    return get_welcome_response()
 
 def on_session_ended(session_ended_request, session):
     """ Called when the user ends the session.
@@ -203,17 +245,17 @@ def lambda_handler(event, context):
     """ Route the incoming request based on type (LaunchRequest, IntentRequest,
     etc.) The JSON body of the request is provided in the event parameter.
     """
-    # print("event.session.application.applicationId=" +
-    #       event['session']['application']['applicationId'])
+    print("event.session.application.applicationId=" +
+          event['session']['application']['applicationId'])
 
     """
     Uncomment this if statement and populate with your skill's application ID to
     prevent someone else from configuring a skill that sends requests to this
     function.
     """
-    # if (event['session']['application']['applicationId'] !=
-    #         "amzn1.echo-sdk-ams.app.[unique-value-here]"):
-    #     raise ValueError("Invalid Application ID")
+    if (event['session']['application']['applicationId'] !=
+            "amzn1.ask.skill.26c78df3-5551-4001-b71b-eb6fdd799ebc"):
+        raise ValueError("Invalid Application ID")
 
     if event['session']['new']:
         on_session_started({'requestId': event['request']['requestId']},
